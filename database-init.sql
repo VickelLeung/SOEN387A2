cae479b8970a94f2c58abfbbfa15f3a8569b6361
LunchBox <vickel1993@hotmail.com>
2019-10-31 21:20:15 -0400

    Update database-init.sql

--- database-init.sql
+++ database-init.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE Book
 
-Create Table book
+Create Table book(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(50),
+  description VARCHAR(200),
+  ISBN VARCHAR(20),
+  UNIQUE KEY unique_isbn (ISBN),
+  author VARCHAR(40),
+  publisher VARCHAR(100),  
+  );
