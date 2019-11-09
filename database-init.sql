1d0406192d76cb5bb6faa0d094102c45ab6f0f53
LunchBox <vickel1993@hotmail.com>
2019-11-08 22:20:36 -0500

    Update database-init.sql

--- database-init.sql
+++ database-init.sql
@@ -11,15 +11,22 @@ Create Table Book(
   UNIQUE KEY unique_isbn (ISBN),
   firstName VARCHAR(100),
   lastName VARCHAR(100),
-  publisher VARCHAR(100)
+  publisher_company VARCHAR(100),
+  publisher_address VARCHAR(100)
   );
   
 --   add BLOB into book table
   ALTER TABLE BOOK ADD COLUMN picture blob;
   
-  
 -- INSERT example
-  INSERT INTO book (title, description, ISBN, firstName, lastName, publisher)
-VALUES ('Cardinal','description of book','S23K45AO','Stavanger','Smith','theMcdonald');
+ INSERT INTO book (title, description, ISBN, firstName, lastName, picture, publisher_company, publisher_address) 
+ VALUES ("The joker", "A scary book about clowns that will murder you.", "J4L 1M5", "Joaquin", "Phoenix", "", "TheGothan", "GothamCity");
+ 
+--  Insert data dummy
+ INSERT INTO book (title, description, ISBN, firstName, lastName, picture, publisher_company, publisher_address) 
+ VALUES ("The dark knight", "A knight that will try to help you do your task such as cleaning.", "L6M 2S8", "Good", "Knight", "", "TheKnight", "Medieval");
+
+ INSERT INTO book (title, description, ISBN, firstName, lastName, picture, publisher_company, publisher_address) 
+ VALUES ("A funny book", "A book that will make you laught.", "H5K 2M1", "Will", "Smith", "", "BlackGuy", "HomieHood");
 
 select * from book;
